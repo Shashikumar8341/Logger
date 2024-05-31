47a5434f7e76d3a5649afb06a85b13f94576007a
shashikumar <shashi.b8341@gmail.com>
2024-05-31 20:04:38 +0530

    Add files via upload

---
+++ LogbackuseXML/src/main/java/com/ciq/LogbackApplication.java
@@ -0,0 +1,16 @@
+package com.ciq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogbackApplication {
+	
+	//Example of Logging Using SLF4J with Logback:
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogbackApplication.class, args);
+	}
+
+}
